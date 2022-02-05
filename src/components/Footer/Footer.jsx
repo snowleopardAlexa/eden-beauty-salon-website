5a4795779b45ab7e1417ea9cfeb0d36815dafd64
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-05 13:17:40 -0500

    style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,83 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubText,
+  FooterSubHeading,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+} from './Footer.elements';
 
-
-const Footer = () => {
+function Footer() {
   return (
-     <FooterContainer>
-         <FooterSubscription>
-             <FooterSubHeading>
-                 hello
-             </FooterSubHeading>
-             <FooterSubText>
-                 hello
-             </FooterSubText>
-         </FooterSubscription>
-     </FooterContainer>
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name='email' type='email' placeholder='Your Email' />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to='/sign-up'>Our Star</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Artists</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Locations</FooterLink>
+            <FooterLink to='/'>Media</FooterLink>
+            <FooterLink to='/'>Partnership</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Hire Us</FooterLinkTitle>
+            <FooterLink to='/'>Request Services</FooterLink>
+            <FooterLink to='/'>Runaway Designers</FooterLink>
+            <FooterLink to='/'>Modeling Agencies</FooterLink>
+            <FooterLink to='/'>Exclusive Stars</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to='/'>Instagram</FooterLink>
+            <FooterLink to='/'>Facebook</FooterLink>
+            <FooterLink to='/'>Youtube</FooterLink>
+            <FooterLink to='/'>Twitter</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+              Eden
+          </SocialLogo>
+          <WebsiteRights>Eden © 2022</WebsiteRights>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
   );
-};
+}
 
 export default Footer;
