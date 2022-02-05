c900c483e047f43aee423001e50a1ce0623e55cb
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-04 22:50:04 -0500

    add img

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
     lightTextDesc: 'Feel confident again after a treatment at Eden Beauty Salon. We will help you to look vital and young. It is time to relax and take care of yourself. We will help you. We will change your world.',
     headline: 'Lead Beauty Salon in New York City',
     buttonLabel: 'Discover more',
-    img: '',
-    alt: 'woman',
+    img: require('../../img/woman.jpg'),
+    alt: 'Woman',
     start: '',
 }
