f4e55d4d7e2bc7d21840561c6219555274b2c40c
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-04 21:56:22 -0500

    add data for display info

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,11 @@
 export const homeObjOne = {
-    lightBg: false
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    topLine: 'Eden Beauty Salon',
+    lightText: true,
+    lightTextDesc: 'We change your world. You can trust us. Feel confident again after taking care of yourself at Eden Beauty Salon. We will help you to always look vital and young.',
+    headline: 'Lead Beauty Salon in New York City',
+    buttonLabel: 'Discover our potential'
 }
