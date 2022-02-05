63fc0b806655433674d63c967dc5ba7a7846f99e
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-04 21:07:24 -0500

    create info section styles and import them to info section comp

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,13 @@
+import { InfoSec } from './InfoSection.elements';
+
+const InfoSection = ({ lightBg }) => {
+  return (
+   <>
+    <InfoSec lightBg={lightBg}>
+
+    </InfoSec>
+   </>
+  );
+};
+
+export default InfoSection;
