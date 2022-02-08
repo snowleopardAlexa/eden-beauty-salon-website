eee8b07e69c175c34ed711134b2026a461f791d4
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-07 19:37:18 -0500

    Add data

--- src/pages/ProductsPage/Data.js
+++ src/pages/ProductsPage/Data.js
@@ -1,14 +1,26 @@
 export const productsObjOne = {
+    topLine: 'Biosilk Silk Theraphy',
+    headline: '#1 Shampoo in the world 2021',
+    description: 'Silk improves...',
+    buttonLabel: 'Order now',
     img: require('../../img/shampoons/biosilk-therapy.jpeg'),
     alt: 'Biosilk',
 }
 
 export const productsObjTwo = {
+    topLine: 'Davines Oi Shampoo',
+    headline: '#2 Shampoo in the world 2021',
+    description: 'Davines...',
+    buttonLabel: 'Order now',
     img: require('../../img/shampoons/davines-oi-shampoo.jpeg'),
     alt: 'Davines',
 }
 
 export const productsObjThree = {
+    topLine: 'Love Shampoo',
+    headline: 'Women love its smell and...',
+    description: '...',
+    buttonLabel: 'Order now',
     img: require('../../img/shampoons/love-shampoo.jpeg'),
     alt: 'Love Shampoo',
 }
