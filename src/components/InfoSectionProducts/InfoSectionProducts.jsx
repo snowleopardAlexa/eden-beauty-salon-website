f05d33dd10aea32bf153e508d475c7349fd3f3ae
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-07 19:18:07 -0500

    add layout products

--- src/components/InfoSectionProducts/InfoSectionProducts.jsx
+++ src/components/InfoSectionProducts/InfoSectionProducts.jsx
@@ -1,7 +1,38 @@
+  
+  
   const InfoSectionProducts = () => {
     return (
       <>
-       products
+         <>
+      <InfoSecProducts>
+        <Container>
+          <ProductsRow imgStart={imgStart}>
+            <ProductsColumn>
+              <ProductsTextWrapper>
+                <ProductsTitle>{topLine}</ProductsTitle>
+                <ProductsHeading>{headline}</ProductsHeading>
+                <ProductsSubtitle>{description}</ProductsSubtitle>
+                  <Link to="" style={{textDecoration: 'none'}}>
+                    <Button 
+                      big 
+                      fontBig
+                       btnBg={btnBg}
+                       color={color}
+                    >
+                      {buttonLabel}
+                    </Button>
+                  </Link>
+              </ProductsTextWrapper>
+            </ProductsColumn>
+            <InfoColumn>
+            <ImgWrapper start={start}>
+              <Img src={img} alt={alt} />
+            </ImgWrapper>
+            </InfoColumn>
+          </ProductsRow>
+        </Container>
+      </InfoSecProducts>
+    </>
       </>
     );
   };
--- src/pages/ProductsPage/Data.js
+++ src/pages/ProductsPage/Data.js
@@ -1,3 +1,14 @@
 export const productsObjOne = {
-   
+    img: require('../../img/shampoons/biosilk-therapy.jpeg'),
+    alt: 'Biosilk',
+}
+
+export const productsObjTwo = {
+    img: require('../../img/shampoons/davines-oi-shampoo.jpeg'),
+    alt: 'Davines',
+}
+
+export const productsObjThree = {
+    img: require('../../img/shampoons/love-shampoo.jpeg'),
+    alt: 'Love Shampoo',
 }
