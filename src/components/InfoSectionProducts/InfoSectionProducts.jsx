0bca18aeb18464e9a4d4e47e762e458236d9c72f
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-07 19:22:44 -0500

    add props

--- src/components/InfoSectionProducts/InfoSectionProducts.jsx
+++ src/components/InfoSectionProducts/InfoSectionProducts.jsx
@@ -1,40 +1,60 @@
-  
-  
-  const InfoSectionProducts = () => {
-    return (
+import {
+  ProductsSec,
+  ProductsRow,
+  InfoColumn,
+  ProductsTextWrapper,
+  ProductsTitle,
+  ProductsHeading,
+  ProductsSubtitle,
+  ProductsColumn,
+  Container,
+  Link,
+  Button,
+  ProductImgWrapper,
+  Img,
+} from "./InfoSectionProducts.elements";
+
+const InfoSectionProducts = ({
+  img,
+  alt,
+  color,
+  start,
+  imgStart,
+  topLine,
+  headline,
+  description,
+  btnBg,
+  buttonLabel,
+}) => {
+  return (
+    <>
       <>
-         <>
-      <InfoSecProducts>
-        <Container>
-          <ProductsRow imgStart={imgStart}>
-            <ProductsColumn>
-              <ProductsTextWrapper>
-                <ProductsTitle>{topLine}</ProductsTitle>
-                <ProductsHeading>{headline}</ProductsHeading>
-                <ProductsSubtitle>{description}</ProductsSubtitle>
-                  <Link to="" style={{textDecoration: 'none'}}>
-                    <Button 
-                      big 
-                      fontBig
-                       btnBg={btnBg}
-                       color={color}
-                    >
+        <ProductsSec>
+          <Container>
+            <ProductsRow imgStart={imgStart}>
+              <ProductsColumn>
+                <ProductsTextWrapper>
+                  <ProductsTitle>{topLine}</ProductsTitle>
+                  <ProductsHeading>{headline}</ProductsHeading>
+                  <ProductsSubtitle>{description}</ProductsSubtitle>
+                  <Link to="" style={{ textDecoration: "none" }}>
+                    <Button big fontBig btnBg={btnBg} color={color}>
                       {buttonLabel}
                     </Button>
                   </Link>
-              </ProductsTextWrapper>
-            </ProductsColumn>
-            <InfoColumn>
-            <ImgWrapper start={start}>
-              <Img src={img} alt={alt} />
-            </ImgWrapper>
-            </InfoColumn>
-          </ProductsRow>
-        </Container>
-      </InfoSecProducts>
-    </>
+                </ProductsTextWrapper>
+              </ProductsColumn>
+              <InfoColumn>
+                <ProductImgWrapper start={start}>
+                  <Img src={img} alt={alt} />
+                </ProductImgWrapper>
+              </InfoColumn>
+            </ProductsRow>
+          </Container>
+        </ProductsSec>
       </>
-    );
-  };
-  
-  export default InfoSectionProducts;
+    </>
+  );
+};
+
+export default InfoSectionProducts;
