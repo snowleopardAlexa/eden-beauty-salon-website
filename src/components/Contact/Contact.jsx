4a10edee247fc003436b4d694926a8bf1ac31c96
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 12:22:06 -0400

    add react hook form syntaxt

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,30 @@
 import { ContainerTitle, ContainerSubtitle, ContainerText, Form, FormGroup, Label, Input, Message } from './Contact.elements';
 import { Container, Button } from '../../globalStyles';
+import { useForm } from 'react-hook-form';
 
 const Contact = ({ title, subtitle, bottomLine }) => {
+
+  // form validation
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Container>
     <ContainerTitle>{title}</ContainerTitle>    
     <ContainerSubtitle>{subtitle}</ContainerSubtitle>
     <ContainerText>{bottomLine}</ContainerText>
-    <Form>
-        <FormGroup>
-            <Label>Name</Label>
-            <Input></Input>
-            <Message></Message>
-        </FormGroup>
-        <FormGroup>
-            <Label>Email</Label>
-            <Input />
-        </FormGroup>
-        <FormGroup>
-            <Label>Message</Label>
-            <Input />
-            <Message></Message>
-            <Button>Send Message</Button>
-        </FormGroup>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Input {...register('Name', { required: true })} />
+      {errors.Name && <p>Name is required.</p>}
+      <Input {...register('Email', { required: true })} />
+      {errors.Email && <p>Email is required.</p>}
+      <Input {...register('Message', { required: true })} />
+      {errors.Message && <p>Message is required.</p>}
+      <Input type="submit" />
     </Form>
     </Container>  
   );
